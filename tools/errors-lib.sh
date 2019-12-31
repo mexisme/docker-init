32638775edf8eba36fc1156c1cfc3354dedb69ae
mexisme <wildjim+dev@kiwinet.org>
2019-12-31 21:59:42 +1300

    Whitespace tidy

--- tools/errors-lib.sh
+++ tools/errors-lib.sh
@@ -3,38 +3,38 @@
 # Write log messages to STDERR, prefixing each item after $1 (i.e. $2...)
 # with the contents of $1:
 log() {
-    logLevel="$1"; shift
-    for message in "$@"; do
-        echo "${logLevel}: ${message}" >&2
-    done
+  logLevel="$1"; shift
+  for message in "$@"; do
+    echo "${logLevel}: ${message}" >&2
+  done
 }
 
 # Exit if $1 != "" or "continue":
 exitp() {
-    [ "$1" == "exit" ] && exit 1
-    [ "$1" == "" ] || [ "$1" == "continue" ] || exit
+  [ "$1" == "exit" ] && exit 1
+  [ "$1" == "" ] || [ "$1" == "continue" ] || exit
 }
 
 # Run the given command (in $2...) and then exit with $1 as the status code:
 eval_exit_code() {
-    statusCode="$1"; shift
+  statusCode="$1"; shift
 
-    "$@"
-    [ "${statusCode}" == "" ] && exit
-    exit "${statusCode}"
+  "$@"
+  [ "${statusCode}" == "" ] && exit
+  exit "${statusCode}"
 }
 
 # "Run" the given args as a command, then exit with status code 1:
 eval_exit() {
-    eval_exit_code 1 "$@"
+  eval_exit_code 1 "$@"
 }
 
 # Log the given args as an Error;  i.e. log each arg, prefixing with "ERR":
 error() {
-    log ERR "$@"
+  log ERR "$@"
 }
 
 # Log the given args as an Error;  i.e. log each arg, prefixing with "WARN":
 warn() {
-    log WARN "$@"
+  log WARN "$@"
 }
