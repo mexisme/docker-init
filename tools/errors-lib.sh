c96fb1d0040dfd6d3c2c4fc3c54e824a52c11b54
mexisme <wildjim+dev@kiwinet.org>
2019-12-31 22:03:08 +1300

    Fix a few logging errors

--- tools/errors-lib.sh
+++ tools/errors-lib.sh
@@ -4,23 +4,23 @@
 # with the contents of $1:
 log() {
   logLevel="$1"; shift
+  [[ "$logLevel" ]] && logLevel="${logLevel}:"
   for message in "$@"; do
-    echo "${logLevel}: ${message}" >&2
+    echo "#${logLevel} ${message}" >&2
   done
 }
 
 # Exit if $1 != "" or "continue":
 exitp() {
-  [ "$1" == "exit" ] && exit 1
-  [ "$1" == "" ] || [ "$1" == "continue" ] || exit
+  [ "$1" = "exit" ] && exit 1
+  [ -z "$1" ] || [ "$1" = "continue" ] || exit
 }
 
 # Run the given command (in $2...) and then exit with $1 as the status code:
 eval_exit_code() {
-  statusCode="$1"; shift
-
-  "$@"
-  [ "${statusCode}" == "" ] && exit
+  statusCode="${1:-0}"; shift
+  [ -n "$*" ] && "$@"
+  [ -n "${statusCode}" ] || exit
   exit "${statusCode}"
 }
 
@@ -37,4 +37,9 @@ error() {
 # Log the given args as an Error;  i.e. log each arg, prefixing with "WARN":
 warn() {
   log WARN "$@"
+}
+
+# Log the given args as an Error;  i.e. log each arg, prefixing with "WARN":
+info() {
+  log "" "$@"
 }
